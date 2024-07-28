ceef93664b68b4f1a4b055325860fba94672dc03
youngjun kim <108052178+kyj4327@users.noreply.github.com>
2024-07-29 01:24:45 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,23 @@
 const BACKEND_API_URL = process.env.REACT_APP_BACKEND_API_URL;
 
-export const fetchDataFromBackend = async (endpoint) => {
+export const fetchDataFromBackend = async (endpoint, options = {}) => {
   try {
-    const response = await fetch(`${BACKEND_API_URL}${endpoint}`);
+    console.log(`Fetching: ${BACKEND_API_URL}${endpoint}`);
+    const response = await fetch(`${BACKEND_API_URL}${endpoint}`, {
+      ...options,
+      headers: {
+        "Content-Type": "application/json",
+        ...options.headers,
+      },
+    });
+    console.log(`Response status: ${response.status}`);
+
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error(
+        `Network response was not ok, status: ${response.status}`
+      );
     }
+
     const data = await response.json();
     return data;
   } catch (error) {
