7a5f8982beb71f5f28e1553a74e8a3d71b43d231
youngjun kim <youngjun4327@gmail.com>
2024-07-29 13:12:01 +0900

    뉴스 새창으로

--- src/components/MainNews.jsx
+++ src/components/MainNews.jsx
@@ -18,12 +18,9 @@ const MainNews = ({ category }) => {
     getNews();
   }, [category]);
 
+  // <br> 태그 제거
   const removeHtmlTags = (str) => {
     return str.replace(/<br\s*\/?>/gi, "");
-  };
-
-  const handleTitleClick = (url) => {
-    navigate("/news", { state: { url, search: category } });
   };
 
   return (
@@ -31,60 +28,55 @@ const MainNews = ({ category }) => {
       <div
         style={{ width: "97%", height: "98.5%", margin: "1.5% 1.5% 0 1.5%" }}
       >
-        {data.map((v, i) => (
-          <div key={i}>
-            <p
-              style={{
-                margin: "0",
-                fontSize: "2.2vmin",
-                fontWeight: "bold",
-              }}
-            >
-              <span
-                onClick={() => handleTitleClick(v.url)}
+        {data.map((v, i) => {
+          return (
+            <div key={i}>
+              <p
                 style={{
-                  cursor: "pointer",
-                  color: "blue",
-                  textDecoration: "underline",
-                }}
-                dangerouslySetInnerHTML={{ __html: v.title }}
-              />
-            </p>
-            {v.urlToImage && v.description && (
-              <div
-                className="content"
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  marginTop: "1.8vh",
+                  margin: "0",
+                  fontSize: "2.2vmin",
+                  fontWeight: "bold",
                 }}
               >
-                {v.urlToImage && (
-                  <img
-                    src={v.urlToImage}
-                    style={{
-                      width: "15%",
-                      height: "20%",
-                      marginRight: "1vw",
-                    }}
-                  />
-                )}
-                {v.description && (
-                  <p
-                    style={{ margin: "0", fontSize: "1.8vmin" }}
-                    dangerouslySetInnerHTML={{
-                      __html: removeHtmlTags(v.description),
-                    }}
-                  ></p>
-                )}
-              </div>
-            )}
-            <hr style={{ margin: "2.5vh 0 2.5vh 0" }} />
-          </div>
-        ))}
+                <a href={v.url} target="_blank">
+                  {v.title}
+                </a>
+              </p>
+              {v.urlToImage && v.description && (
+                <div
+                  className="content"
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    marginTop: "1.8vh",
+                  }}
+                >
+                  {v.urlToImage && (
+                    <img
+                      src={v.urlToImage}
+                      style={{
+                        width: "15%",
+                        height: "20%",
+                        marginRight: "1vw",
+                      }}
+                    />
+                  )}
+                  {v.description && (
+                    <p
+                      style={{ margin: "0", fontSize: "1.8vmin" }}
+                      dangerouslySetInnerHTML={{
+                        __html: removeHtmlTags(v.description),
+                      }}
+                    ></p>
+                  )}
+                </div>
+              )}
+              <hr style={{ margin: "2.5vh 0 2.5vh 0" }} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 };
-
 export default MainNews;
