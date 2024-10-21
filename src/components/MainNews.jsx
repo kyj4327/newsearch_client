2486dc587ec8a40a41afdaab3e4fe4ef0b25f6e0
youngjun kim <youngjun4327@gmail.com>
2024-10-21 19:16:36 +0900

    한국->미국 으로 변경

--- src/components/MainNews.jsx
+++ src/components/MainNews.jsx
@@ -1,26 +1,25 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { fetchDataFromBackend } from "../api"; // fetchDataFromBackend 함수 가져오기
 
 const MainNews = ({ category }) => {
   const [data, setData] = useState([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const getNews = async () => {
-      try {
-        const res = await fetchDataFromBackend(`/news?category=${category}`);
-        setData(res.articles);
-      } catch (error) {
-        console.error("Error fetching news:", error);
-      }
+      const cate = category === "all" ? "" : `&category=${category}`;
+      const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+      // const url = `https://newsapi.org/v2/top-headlines?country=kr${cate}&apiKey=&{apiKey}`;
+      // const url = `https://newsapi.org/v2/top-headlines?country=kr${cate}&apiKey=3edbe8fb44eb4142988957f180c08ef1`;
+      const url = `https://newsapi.org/v2/top-headlines?country=us${cate}&apiKey=4d04ef5559d647efa5e26f934f7db879`;
+      const res = await fetch(url);
+      const datas = await res.json();
+      setData(datas.articles);
     };
     getNews();
   }, [category]);
 
   // <br> 태그 제거
   const removeHtmlTags = (str) => {
-    return str.replace(/<br\s*\/?>/gi, "");
+    return str.replace(/<br\s*\/?>/gi, '');
   };
 
   return (
@@ -42,36 +41,24 @@ const MainNews = ({ category }) => {
                   {v.title}
                 </a>
               </p>
-              {v.urlToImage && v.description && (
-                <div
-                  className="content"
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    marginTop: "1.8vh",
-                  }}
-                >
-                  {v.urlToImage && (
-                    <img
-                      src={v.urlToImage}
-                      style={{
-                        width: "15%",
-                        height: "20%",
-                        marginRight: "1vw",
-                      }}
-                    />
-                  )}
-                  {v.description && (
-                    <p
-                      style={{ margin: "0", fontSize: "1.8vmin" }}
-                      dangerouslySetInnerHTML={{
-                        __html: removeHtmlTags(v.description),
-                      }}
-                    ></p>
-                  )}
-                </div>
-              )}
-              <hr style={{ margin: "2.5vh 0 2.5vh 0" }} />
+              {
+                v.urlToImage && v.description && (
+                  <div className="content" style={{ display: "flex", alignItems: "center", marginTop: "1.8vh" }}>
+                    {v.urlToImage && (
+                      <img
+                        src={v.urlToImage}
+                        style={{
+                          width: "15%",
+                          height: "20%",
+                          marginRight: "1vw"
+                        }}
+                      />
+                    )}
+                    {v.description && <p style={{ margin: "0", fontSize: "1.8vmin" }} dangerouslySetInnerHTML={{ __html: removeHtmlTags(v.description) }}></p>}
+                  </div>
+                )
+              }
+              <hr style={{ margin: '2.5vh 0 2.5vh 0' }} />
             </div>
           );
         })}
